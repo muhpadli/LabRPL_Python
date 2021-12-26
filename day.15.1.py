0a56caefe413d1226b96d1724943785bcfec60e3
muhpadli <muhp5934@gmail.com>
2021-12-26 22:03:24 +0700

    first commit

---
+++ day.15.1.py
@@ -0,0 +1,16 @@
+print(9*"=","belajar for else, range, break",9*"=")
+
+number = int(input("masukkan angka yang dicari <50 :"))
+
+for i in range(0, 50):
+    print(i)
+    if i == number :
+        print("nilai yang dicari adalah",i)
+        break
+else:
+    print("angka tidak ditemukan")
+print("pencarian angka berakhir")
+    
+print(12*"=","belajar python itu mudah",12*"=")
+            
+    
