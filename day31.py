d0678509c3674b82749ab785fc86f07c50b1f073
muhpadli <muhp5934@gmail.com>
2022-01-11 07:33:08 +0700

    Aritmatika dan if else

---
+++ day31.py
@@ -0,0 +1,27 @@
+#program menghitung gaji total
+golongan = int(input("masukkan golongan anda (1,2,3) :"))
+tahun_kerja = int(input("Masukkan tahun mulai kerja :"))
+masa_kerja = 2011 - tahun_kerja
+
+if(tahun_kerja >= 7):
+    bonus = 150_000
+    if(golongan ==1):
+        gaji_pokok = 1_500_000
+    elif(golongan ==2):
+        gaji_pokok = 1_200_000
+    elif(golongan ==1):
+        gaji_pokok = 1_000_000
+else:
+    bonus = 0
+    if(golongan ==1):
+        gaji_pokok = 1_500_000
+    elif(golongan ==2):
+        gaji_pokok = 1_200_000
+    elif(golongan ==1):
+        gaji_pokok = 1_000_000
+
+print(f"Gaji total anda adalah Rp. {gaji_pokok+bonus} ")
+
+
+    
+
