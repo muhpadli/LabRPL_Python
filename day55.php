c349fd9e33d6ceaee2a83d68aa1cb4594757e916
muhpadli <muhp5934@gmail.com>
2022-02-04 21:39:41 +0700

    day 55

---
+++ day55.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title> Program Perkalian</title>
+</head>
+
+<body>
+    <h3>Program Perkalian</h3>
+    <ol>
+        <?php
+        for ($i = 1; $i <= 10; $i++) {
+            $hasil = $i * 2;
+            echo "<li>$i x 2 = $hasil </li>";
+        }
+        ?>
+    </ol>
+</body>
+
+</html>
